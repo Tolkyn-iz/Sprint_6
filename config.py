3c716fed1cc646ce50c9f4fee700331b57a25b71
Tolkyn <iz.tolkyn@gmail.com>
2026-04-18 16:20:36 +0500

    fix: исправление замечаний из ревью - добавлен .gitignore - убраны хардкод пути и URL - добавлен модуль data для тестовых данных - заменены прямые вызовы driver на методы BasePage - убраны print и time.sleep - добавлены allure.step

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+import os
+
+class Config:
+    # Путь к geckodriver
+    GECKODRIVER_PATH = r'C:\Users\daniy\Documents\geckodriver-v0.36.0-win32\geckodriver.exe'
+    
+    # Путь к Firefox
+    FIREFOX_PATH = r'C:\Program Files\Mozilla Firefox\firefox.exe'
--- tests/test_order.py
+++ tests/test_order.py
@@ -3,30 +3,38 @@ import pytest
 from pages.home_page import HomePage
 from pages.order_page import OrderPage
 from pages.order_success_page import OrderSuccessPage
+from data import ORDER_DATA_TOP_BUTTON, ORDER_DATA_BOTTOM_BUTTON
+
 
 @allure.feature("Заказ самоката")
 class TestOrder:
     
-    @allure.title("Позитивный сценарий заказа самоката")
-    @pytest.mark.parametrize("button_location, name, surname, address, metro_station, phone, "
-                             "delivery_date, rental_period, color, comment", [
-        ("top", "Анна", "Петрова", "ул. Ленина 10", "Кропоткинская", 
-         "+79161234567", "25.10.2024", "сутки", "black", "Позвоните за час"),
-        
-        ("bottom", "Сергей", "Иванов", "пр. Мира 25", "Комсомольская", 
-         "+79998887766", "26.10.2024", "трое суток", "grey", "Домофон 123")
-    ])
-    def test_create_order(self, driver, button_location, name, surname, address, 
-                          metro_station, phone, delivery_date, rental_period, 
-                          color, comment):
-        
+    @allure.title("Позитивный сценарий заказа самоката через верхнюю кнопку")
+    @pytest.mark.parametrize("name, surname, address, metro_station, phone, "
+                             "delivery_date, rental_period, color, comment", 
+                             ORDER_DATA_TOP_BUTTON)
+    def test_create_order_top_button(self, driver, name, surname, address,
+                                      metro_station, phone, delivery_date, 
+                                      rental_period, color, comment):
         home_page = HomePage(driver)
+        home_page.click_order_button_top()
         
-        # Выбор кнопки заказа в зависимости от параметра
-        if button_location == "top":
-            home_page.click_order_button_top()
-        else:
-            home_page.click_order_button_bottom()
+        order_page = OrderPage(driver)
+        order_page.create_order(name, surname, address, metro_station, phone,
+                               delivery_date, rental_period, color, comment)
+        
+        success_page = OrderSuccessPage(driver)
+        assert success_page.is_order_success_displayed(), "Заказ не был оформлен успешно"
+    
+    @allure.title("Позитивный сценарий заказа самоката через нижнюю кнопку")
+    @pytest.mark.parametrize("name, surname, address, metro_station, phone, "
+                             "delivery_date, rental_period, color, comment", 
+                             ORDER_DATA_BOTTOM_BUTTON)
+    def test_create_order_bottom_button(self, driver, name, surname, address,
+                                         metro_station, phone, delivery_date, 
+                                         rental_period, color, comment):
+        home_page = HomePage(driver)
+        home_page.click_order_button_bottom()
         
         order_page = OrderPage(driver)
         order_page.create_order(name, surname, address, metro_station, phone,
